5e43726993a339b786b703f12f05ef3e6f0b0565
Xtreme-654 <xtremedosamba@gmail.com>
2025-11-15 14:57:13 -0300

    Banco de dados concertado, com constraints, auto_increment e colunas null/not_null

--- Script_projeto-individual.sql
+++ Script_projeto-individual.sql
@@ -1,37 +1,46 @@
+create database projeto_individual;
+
 use projeto_individual;
 
+CREATE TABLE `mensage_usuario`(
+`id_mensagem` INT PRIMARY KEY AUTO_INCREMENT,
+`email` VARCHAR(100) NOT NULL,
+`mensagem` VARCHAR(500) NOT NULL);
+ 
 CREATE TABLE `usuario` (
-  `id_cadastro` INT PRIMARY KEY NOT NULL,
+  `id_cadastro` INT PRIMARY KEY AUTO_INCREMENT,
   `apelido` VARCHAR(50) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
-  `dt_cadastro` DATE NOT NULL,
-  `usuariocol` VARCHAR(45) NULL);
+  `dt_cadastro` DATETIME DEFAULT CURRENT_TIMESTAMP);
 
 
 CREATE TABLE `jogo` (
-  `id_jogo` INT PRIMARY KEY NOT NULL,
+  `id_jogo` INT PRIMARY KEY AUTO_INCREMENT,
   `capa` VARCHAR(500) NULL,
   `nome` VARCHAR(50) NOT NULL,
   `genero` VARCHAR(45) NOT NULL,
-  `classificacao` INT NOT NULL,
+  CONSTRAINT`chk_genero` CHECK(`genero` IN('rpg','acao','esporte','estrategia')),
+  `classificacao` INT NULL,
+  CONSTRAINT `chk_classificacao`
+  CHECK(`classificacao` IN('10','14','16','18')),
   `dt_lancamento` DATE NOT NULL,
   `publicadora` VARCHAR(100) NOT NULL,
   `desenvolvedora` VARCHAR(100) NOT NULL,
   `valor` DECIMAL(5,2) NULL,
   `peso` DECIMAL(5,2) NOT NULL,
-  `online` TINYINT NOT NULL,
-  `crossplay` TINYINT NOT NULL);
+  `online` TINYINT NULL,
+  `crossplay` TINYINT NULL);
 
 
 CREATE TABLE `biblioteca_de_jogos` (
-  `id_biblioteca` INT NOT NULL,
+  `id_biblioteca` INT AUTO_INCREMENT,
   `fk_usuario` INT NOT NULL,
   `fk_jogo` INT NOT NULL,
   `horas_jogo` DATE NULL,
-  `dt_compra` DATE NOT NULL,
+  `dt_compra` DATE NULL,
   `versao` CHAR(3) NOT NULL,
-  `lista_desejos` TINYINT NOT NULL,
+  `lista_desejos` TINYINT NULL,
   PRIMARY KEY (`id_biblioteca`, `fk_usuario`, `fk_jogo`),
   CONSTRAINT `fk_biblioteca_de_jogos_cadastro`
     FOREIGN KEY (`fk_usuario`)
@@ -43,7 +52,7 @@ CREATE TABLE `biblioteca_de_jogos` (
 
 
 CREATE TABLE `conteudo_adicional` (
-  `id_conteudo_adicional` INT NOT NULL,
+  `id_conteudo_adicional` INT AUTO_INCREMENT,
   `fk_jogo` INT NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `dt_lancamento` DATE NOT NULL,
@@ -54,12 +63,16 @@ CREATE TABLE `conteudo_adicional` (
     REFERENCES `jogo` (`id_jogo`));
 
 CREATE TABLE `conquista` (
-  `id_conquista` INT NOT NULL,
+  `id_conquista` INT AUTO_INCREMENT,
   `fk_jogo` INT NOT NULL,
   `titulo` VARCHAR(45) NOT NULL,
   `raridade` VARCHAR(45) NOT NULL,
+  CONSTRAINT `chk_raridade` 
+  CHECK(`raridade` IN('comum','raro','muito raro','ultra raro')),
   `descricao` VARCHAR(500) NULL,
   `grau` VARCHAR(45) NULL,
+  CONSTRAINT `chk_grau`
+  CHECK (`grau` IN('bronze','prata','ouro','platina')),
   PRIMARY KEY (`id_conquista`, `fk_jogo`),
   CONSTRAINT `fk_conquista_jogo`
     FOREIGN KEY (`fk_jogo`)
